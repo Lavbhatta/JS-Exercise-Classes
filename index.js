4af3f139952f0ec05f856990fd4d49185e85ced4
Lav-Bhatta <lavbhatta@gmail.com>
2020-10-15 22:09:09 -0500

    Final

--- index.js
+++ index.js
@@ -172,7 +172,21 @@ grade(student, subject){
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
+class Student extends Lambdasian {
+constructor(obj2){
+super(obj2)
+this.previousBackground=obj2.previousBackground;
+this.className=obj2.className;
+this.favSubjects=obj2.favSubjects;
+}
+listSubjects(){
+  return `${this.favSubjects}`;
+}
+PRAssignmnet(subject){
+return `${Student.name} has submitted a PR for ${subject}` 
+}
+
+
 
 }
 
@@ -189,8 +203,19 @@ class Student {
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
+class ProjectManager extends Instructor{
+constructor(obj3){
+  super(obj3)
+  this.gradClassName = obj3.gradClassName;
+  this.favInstructor =obj3.favInstructor;
+}
+standup(channel){
+  return `${this.name} announces to ${channel}, @channel standy times!`
+}
+debugsCode(student, subject){
+  return `${name} debugs ${student.name}'s code on ${subject}`
 
+}
 }
 
 /*
